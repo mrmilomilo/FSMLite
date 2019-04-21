52d22b2550bc2a4097acd82196f2fa6a65d7122f
mrmilomilo <49833750+mrmilomilo@users.noreply.github.com>
2019-04-21 18:35:14 +0200

    initial commit
    minimal and efficient plugin helping in creating fsm in ue4

---
+++ Source/LiteFSM/Private/FSMLiteComponent.cpp
@@ -0,0 +1,95 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FSMLiteComponent.h"
+
+// Sets default values for this component's properties
+UFSMLiteComponent::UFSMLiteComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+
+	this->CurState = 0;
+	this->NumOfStates = 0;
+
+	this->bPrintErrorsToLog = true;
+}
+
+
+// Called when the game starts
+void UFSMLiteComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SetComponentTickEnabled(false);
+	
+}
+
+
+// Called every frame
+void UFSMLiteComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	OnUpdateState.Broadcast(CurState);
+}
+
+bool UFSMLiteComponent::InitFSMLite(int32 NumOfStates, uint8 InitialState)
+{
+	if (NumOfStates < 0 || NumOfStates > 255) {
+		
+		if(bPrintErrorsToLog)
+			UE_LOG(LogTemp, Warning, TEXT("FSMLite.InitFSMLite(): invalid NumOfStates!"));
+		
+		return false;
+	}
+	else {
+		this->NumOfStates = NumOfStates;
+	}
+
+
+	if (!IsValidState(InitialState)) {
+		
+		if(bPrintErrorsToLog)
+			UE_LOG(LogTemp, Warning, TEXT("FSMLite.InitFSMLite(): invalid InitialState!"));
+		
+		return false;
+	}
+	else {
+		//this->CurState = InitialState;
+		SetState(InitialState);
+	}
+
+	
+	IsInitialized = true;
+	SetComponentTickEnabled(true);
+	
+	return true;
+}
+
+bool UFSMLiteComponent::SetState(uint8 State)
+{
+	if (!IsValidState(State))
+		return false;
+
+	if (IsInitialized) {
+		OnExitState.Broadcast(CurState);
+	}
+
+	CurState = State;
+
+	OnBeginState.Broadcast(CurState);
+
+	return true;
+}
+
+bool UFSMLiteComponent::IsValidState(uint8 State)
+{
+	if (NumOfStates > 0) {
+		return (State >= 0 && State < this->NumOfStates);
+	}
+	
+	return false;
+}
+
---
+++ Source/LiteFSM/Public/FSMLiteComponent.h
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "FSMLiteComponent.generated.h"
+
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnBeginState, uint8, State);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnTickState, uint8, State);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnEndState, uint8, State);
+
+
+
+UCLASS( Blueprintable, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class LITEFSM_API UFSMLiteComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UFSMLiteComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	
+private:
+	int32 NumOfStates;
+	uint8 CurState;
+
+	bool IsInitialized;
+
+public:
+	UFUNCTION(BlueprintCallable, Category="FSMLite")
+	virtual bool InitFSMLite(int32 NumOfStates, uint8 InitialState);
+
+	UFUNCTION(BlueprintCallable, Category = "FSMLite")
+	virtual bool SetState(uint8 State);
+
+	UFUNCTION(BlueprintCallable, Category = "FSMLite")
+	bool IsValidState(uint8 State);
+
+
+
+
+	UPROPERTY(BlueprintAssignable, Category = "FSMLite")
+		FOnBeginState OnBeginState;
+
+	UPROPERTY(BlueprintAssignable, Category = "FSMLite")
+		FOnTickState OnUpdateState;
+
+	UPROPERTY(BlueprintAssignable, Category = "FSMLite")
+		FOnEndState OnExitState;
+
+
+	UPROPERTY(BlueprintReadWrite, Category = "FSMLite")
+		bool bPrintErrorsToLog;
+};
