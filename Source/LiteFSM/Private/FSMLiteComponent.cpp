b1ac631c3078dbc09861fcac49d578f446eeffd2
C1p86 <andrea@ruggeriandrea.it>
2022-03-10 01:12:29 +0100

    Updated to UE5

--- Source/LiteFSM/Private/FSMLiteComponent.cpp
+++ Source/LiteFSM/Private/FSMLiteComponent.cpp
@@ -8,34 +8,27 @@ UFSMLiteComponent::UFSMLiteComponent()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-
+	
 	this->CurState = 0;
 	this->NumOfStates = 0;
-
 	this->bPrintErrorsToLog = true;
 }
-
 
 // Called when the game starts
 void UFSMLiteComponent::BeginPlay()
 {
 	Super::BeginPlay();
-
 	SetComponentTickEnabled(false);
-	
 }
-
 
 // Called every frame
 void UFSMLiteComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
 	OnTickState.Broadcast(CurState);
 }
 
-bool UFSMLiteComponent::InitFSMLite(int32 NumberOfStates, uint8 InitialState)
+bool UFSMLiteComponent::InitFSMLite(const int32 NumberOfStates, const uint8 InitialState)
 {
 	if (NumberOfStates < 0 || NumberOfStates > 255) {
 		
@@ -44,31 +37,21 @@ bool UFSMLiteComponent::InitFSMLite(int32 NumberOfStates, uint8 InitialState)
 		
 		return false;
 	}
-	else {
-		this->NumOfStates = NumberOfStates;
-	}
+	this->NumOfStates = NumberOfStates;
 
 
 	if (!IsValidState(InitialState)) {
-		
-		if(bPrintErrorsToLog)
-			UE_LOG(LogTemp, Warning, TEXT("FSMLite.InitFSMLite(): invalid InitialState!"));
-		
+		if(bPrintErrorsToLog) UE_LOG(LogTemp, Warning, TEXT("FSMLite.InitFSMLite(): invalid InitialState!"));
 		return false;
 	}
-	else {
-		//this->CurState = InitialState;
-		SetState(InitialState);
-	}
-
 	
+	SetState(InitialState);
 	IsInitialized = true;
 	SetComponentTickEnabled(true);
-	
 	return true;
 }
 
-bool UFSMLiteComponent::SetState(uint8 State)
+bool UFSMLiteComponent::SetState(const uint8 State)
 {
 	if (!IsValidState(State))
 		return false;
@@ -78,29 +61,26 @@ bool UFSMLiteComponent::SetState(uint8 State)
 	}
 
 	CurState = State;
-
 	CurStateBeginTime = GetOwner()->GetWorld()->GetTimeSeconds();	//save game time when cur state starts
-
 	OnBeginState.Broadcast(CurState);
-
 	return true;
 }
 
-bool UFSMLiteComponent::IsValidState(uint8 State)
+bool UFSMLiteComponent::IsValidState(const uint8 State) const
 {
 	if (NumOfStates > 0) {
-		return (State >= 0 && State < this->NumOfStates);
+		return (State < this->NumOfStates);
 	}
 	
 	return false;
 }
 
-uint8 UFSMLiteComponent::GetCurrentState()
+uint8 UFSMLiteComponent::GetCurrentState() const
 {
 	return CurState;
 }
 
-float UFSMLiteComponent::GetTime()
+float UFSMLiteComponent::GetTime() const
 {
 	return (GetOwner()->GetWorld()->GetTimeSeconds() - CurStateBeginTime);
 }
--- Source/LiteFSM/Public/FSMLiteComponent.h
+++ Source/LiteFSM/Public/FSMLiteComponent.h
@@ -4,16 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
-#include "GameFramework/Actor.h"
-#include "Runtime/Engine/Classes/Engine/World.h"
 #include "FSMLiteComponent.generated.h"
-
 
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnBeginState, uint8, State);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnTickState, uint8, State);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnEndState, uint8, State);
-
 
 
 UCLASS( Blueprintable, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
@@ -37,40 +33,34 @@ public:
 private:
 	int32 NumOfStates;
 	uint8 CurState;
-
 	float CurStateBeginTime;
-
 	bool IsInitialized;
 
 public:
 	UFUNCTION(BlueprintCallable, Category="FSMLite")
-	virtual bool InitFSMLite(int32 NumberOfStates, uint8 InitialState);
+	virtual bool InitFSMLite(const int32 NumberOfStates, const uint8 InitialState);
 
 	UFUNCTION(BlueprintCallable, Category = "FSMLite")
-	virtual bool SetState(uint8 State);
+	virtual bool SetState(const uint8 State);
 
 	UFUNCTION(BlueprintPure, Category = "FSMLite")
-	bool IsValidState(uint8 State);
+	bool IsValidState(const uint8 State) const;
 
 	UFUNCTION(BlueprintPure, Category = "FSMLite")
-		uint8 GetCurrentState();
+	uint8 GetCurrentState() const;
 
 	UFUNCTION(BlueprintPure, Category = "FSMLite")
-		float GetTime();
-
-
-
+	float GetTime() const;
+	
+	UPROPERTY(BlueprintAssignable, Category = "FSMLite")
+	FOnBeginState OnBeginState;
 
 	UPROPERTY(BlueprintAssignable, Category = "FSMLite")
-		FOnBeginState OnBeginState;
+	FOnTickState OnTickState;
 
 	UPROPERTY(BlueprintAssignable, Category = "FSMLite")
-		FOnTickState OnTickState;
-
-	UPROPERTY(BlueprintAssignable, Category = "FSMLite")
-		FOnEndState OnEndState;
-
-
+	FOnEndState OnEndState;
+	
 	UPROPERTY(BlueprintReadWrite, Category = "FSMLite")
-		bool bPrintErrorsToLog;
+	bool bPrintErrorsToLog;
 };
